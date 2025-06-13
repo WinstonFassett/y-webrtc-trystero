9dbda54d1e9f6f2f8f56c82e74b047d9f8cc5c46
Winston Fassett <winston.fassett@gmail.com>
2025-06-13 10:02:47 -0500

    demo tweaks

--- demo/index.js
+++ demo/index.js
@@ -1,22 +1,61 @@
 /* eslint-env browser */
 
-import { doc, trysteroProvider as provider } from './sharedTypes.js'
+import { 
+  doc, 
+  trysteroProvider as provider,
+  drawingContent,
+  prosemirrorEditorContent,
+  permanentUserData 
+} from './sharedTypes.js'
 import * as drawing from './drawing.js'
 import { drawingDemo } from './elements.js'
 
-const yarray = doc.getArray()
+// Initialize the drawing array
+const yarray = doc.getArray('demo-array')
 
+// Log connection events
 provider.on('synced', synced => {
-  // NOTE: This is only called when a different browser connects to this client
-  // Windows of the same browser communicate directly with each other
-  // Although this behavior might be subject to change.
-  // It is better not to expect a synced event when using y-trystero
-  console.log('synced!', synced)
+  console.log('Synced with remote peers:', synced)
+  console.log('Room ID:', provider.roomName)
+  console.log('Client ID:', doc.clientID)
 })
 
+// Log when peers connect/disconnect
+provider.on('peers', ({ added, removed }) => {
+  if (added.length) console.log('Peers connected:', added)
+  if (removed.length) console.log('Peers disconnected:', removed)
+})
+
+// Log array changes
 yarray.observeDeep(() => {
-  console.log('yarray updated: ', yarray.toJSON())
+  console.log('Array updated:', yarray.toJSON())
 })
 
-// @ts-ignore
-window.example = { provider, doc, yarray, drawing, drawingDemo }
+// Add some initial data if array is empty
+if (yarray.length === 0) {
+  yarray.insert(0, ['Welcome to y-trystero demo!'])
+}
+
+// Make objects available in console for debugging
+Object.assign(window, {
+  ytrystero: {
+    provider,
+    doc,
+    yarray,
+    drawing,
+    drawingDemo,
+    drawingContent,
+    prosemirrorEditorContent,
+    permanentUserData
+  },
+  // Keep backward compatibility
+  example: { 
+    provider, 
+    doc, 
+    yarray, 
+    drawing, 
+    drawingDemo 
+  }
+})
+
+console.log('y-trystero demo initialized. Access objects via `ytrystero` in the console.')
